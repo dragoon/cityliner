e5b01ee22aadbb79bd9af32866e11f89c2a28e23
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-08-26 22:02:24 +0200

    dd lato fonts, ulm logo

--- citylines/generate_poster.py
+++ citylines/generate_poster.py
@@ -33,32 +33,30 @@ def draw_svg_on_pdf(canvas, svg_path, x, y, height):
     return drawing.width, drawing.height
 
 
-def draw_poster(input_dir: Path, cities: list[str], provider_icon: str | None, size_x=9933, size_y=14043, poster=False):
+def draw_single_poster(input_dir: Path, city: str, logo_name: str, size_x=9933, size_y=14043, poster=False):
     if poster:
         size_x, size_y = 9933, 14043
-    if provider_icon:
-        provider_icon_path = f"{provider_icon}.svg"
-    # pdfmetrics.registerFont(TTFont('Lato', 'Lato.ttf'))
+    pdfmetrics.registerFont(TTFont('Lato', 'assets/fonts/Lato-Regular.ttf'))
 
     c = canvas.Canvas(f"output.pdf", pagesize=(size_x, size_y))
     c.setLineWidth(1)
     c.setFillColorRGB(0.1, 0.1, 0.1)
     c.rect(0, 0, size_x, size_y, fill=1)
 
-    maxmin = load_lines(input_dir, cities)
-    draw_routes(c, input_dir, cities, maxmin)
+    maxmin = load_lines(input_dir, city)
+    draw_routes(c, input_dir, city, maxmin)
 
     # Handle the poster condition
     if poster:
         # c.setFont("Lato", 88)  # This sets the font to Lato with size 88
         c.setFont("Helvetica", 88)
         provider_h = 564
-        provider_w, provider_h = draw_svg_on_pdf(c, f"assets/logos/berlin/berlin.svg", 200, 100, height=provider_h)
+        provider_w, provider_h = draw_svg_on_pdf(c, f"assets/logos/{city}/{logo_name}.svg", 200, 100, height=provider_h)
 
         gray_value = 120 / 255
         c.setFillColorRGB(gray_value, gray_value, gray_value)
         # # Loading the text file and writing the lines
-        with open(f"texts/berlin.txt", "r") as file:
+        with open(f"texts/{city}.txt", "r") as file:
             lines = file.readlines()
             start = 120
             for i, line in enumerate(lines):
@@ -73,13 +71,10 @@ def draw_poster(input_dir: Path, cities: list[str], provider_icon: str | None, s
     c.save()
 
 
-def load_lines(input_dir: Path, cities) -> list:
-    maxmin = []
-    for city in cities:
-        with open(input_dir / city / "maxmin.lines", 'r') as file:
-            max_val = float(file.readline().strip())
-            maxmin.append(max_val)
-    return maxmin
+def load_lines(input_dir: Path, city) -> float:
+    with open(input_dir / city / "maxmin.lines", 'r') as file:
+        max_val = float(file.readline().strip())
+        return max_val
 
 
 def get_route_color(route_type: str) -> Color:
@@ -104,48 +99,47 @@ def get_route_color(route_type: str) -> Color:
             raise ValueError(f"Unknown route type: {route_type}")
 
 
-def draw_routes(c, input_dir: Path, cities, maxmin):
+def draw_routes(c, input_dir: Path, city: str, maxmin: float):
     c.saveState()
     c.translate(0, A0[1] * 2)
     c.scale(1, -1)
     c.translate(0, -2 * A0[1])
 
-    for city in cities:
-        with open(input_dir / city / "data.lines", 'r') as file:
-            for lineS in file:
-                line = lineS.split("\t")
-                trips = line[0]
-                route_types = line[1].split(",")
+    with open(input_dir / city / "data.lines", 'r') as file:
+        for lineS in file:
+            line = lineS.split("\t")
+            trips = line[0]
+            route_types = line[1].split(",")
 
-                for route_type in route_types:
-                    color = get_route_color(route_type)
-                    points = line[2].split(",")
+            for route_type in route_types:
+                color = get_route_color(route_type)
+                points = line[2].split(",")
 
-                    factor = 1.7
-                    stroke_weight = math.log(float(trips) * factor) * 3
-                    if stroke_weight < 0:
-                        stroke_weight = 1.0 * factor
+                factor = 1.7
+                stroke_weight = math.log(float(trips) * factor) * 3
+                if stroke_weight < 0:
+                    stroke_weight = 1.0 * factor
 
-                    c.setLineWidth(stroke_weight)
-                    alph = 100 * (float(trips) / maxmin[0])
-                    if alph < 20.0:
-                        alph = 20.0
-                    color_alpha = Color(color.red, color.green, color.blue, alph / 255.0)
+                c.setLineWidth(stroke_weight)
+                alph = 100 * (float(trips) / maxmin)
+                if alph < 20.0:
+                    alph = 20.0
+                color_alpha = Color(color.red, color.green, color.blue, alph / 255.0)
 
-                    c.setStrokeColor(color_alpha)
-                    c.setLineCap(2)  # square
+                c.setStrokeColor(color_alpha)
+                c.setLineCap(2)  # square
 
-                    path = c.beginPath()
-                    for index, point in enumerate(points):
-                        x, y = float(point.split(" ")[0]), float(point.split(" ")[1])
-                        if index == 0:
-                            path.moveTo(x, y)
-                        else:
-                            path.lineTo(x, y)
-                    c.drawPath(path)
-                    path.close()
+                path = c.beginPath()
+                for index, point in enumerate(points):
+                    x, y = float(point.split(" ")[0]), float(point.split(" ")[1])
+                    if index == 0:
+                        path.moveTo(x, y)
+                    else:
+                        path.lineTo(x, y)
+                c.drawPath(path)
+                path.close()
     c.restoreState()
 
 
 if __name__ == "__main__":
-    draw_poster(Path("."), cities=["ulm"], provider_icon=None, poster=True)
+    draw_single_poster(Path("."), city="ulm", logo_name="swu", poster=True)
