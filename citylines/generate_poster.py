26e74d83ea565b8a503bdb180370ae83271061d2
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-08-26 10:25:37 +0200

    add proper text

--- citylines/generate_poster.py
+++ citylines/generate_poster.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from reportlab.lib import colors
 from reportlab.lib.enums import TA_RIGHT
 from reportlab.lib.pagesizes import A0
 from reportlab.pdfbase import pdfmetrics
@@ -33,25 +34,25 @@ def draw_svg_on_pdf(canvas, svg_path, x, y, width=None, height=None):
 
 def draw_poster(input_dir: Path, cities: list[str], provider_icon: str | None, size_x=9933, size_y=14043, poster=False):
     if poster:
-        size_x, size_y = A0
+        size_x, size_y = 9933, 14043
     if provider_icon:
         provider_icon_path = f"{provider_icon}.svg"
-    # pdfmetrics.registerFont(TTFont('Lato', 'Lato.ttf'))
+    #pdfmetrics.registerFont(TTFont('Lato', 'Lato.ttf'))
 
     c = canvas.Canvas(f"output.pdf", pagesize=(size_x, size_y))
     c.setLineWidth(1)
     c.setFillColorRGB(0.1, 0.1, 0.1)
     c.rect(0, 0, size_x, size_y, fill=1)
-    c.translate(50, 50)
 
     maxmin = load_lines(input_dir, cities)
     draw_routes(c, input_dir, cities, maxmin)
 
     # Handle the poster condition
     if poster:
-        c.setFont("Lato", 88)  # This sets the font to Lato with size 88
+        #c.setFont("Lato", 88)  # This sets the font to Lato with size 88
+        c.setFont("Helvetica", 100)
         # Drawing the provider_icon (adapted for image drawing in reportlab)
-        # provider_h = 564.0
+        provider_h = 564.0
         # provider_w = provider_icon.width * provider_h / provider_icon.height
         # draw_svg_on_pdf(c, provider_icon_path, 200, size_y - provider_h - 190, provider_w, provider_h)
         #
@@ -62,11 +63,13 @@ def draw_poster(input_dir: Path, cities: list[str], provider_icon: str | None, s
         #     c.drawString(380 + provider_w, size_y - 600, text)
 
         # Adding additional text on the poster
+        gray_value = 120 / 255
+        c.setFillColorRGB(gray_value, gray_value, gray_value)
         text_object = c.beginText()
-        text_object.setTextOrigin(size_x, size_y)  # Set the starting position of the text.
-        text_object.setTextAlign(TA_RIGHT)
+        text_object.setTextOrigin(200, 200)
         text_to_add = "\n\nGenerated on cityliner.io\nLicense Creative Commons Attribution 4.0 Unported"
-        c.drawString(size_x - 1500, size_y - 600, text_to_add)
+        text_object.textLines(text_to_add)
+        c.drawText(text_object)
 
     c.showPage()
     c.save()
@@ -105,6 +108,9 @@ def get_route_color(route_type: str) -> Color:
 
 def draw_routes(c, input_dir: Path, cities, maxmin):
     c.saveState()
+    c.translate(0, A0[1]*2)
+    c.scale(1, -1)
+    c.translate(0, -2*A0[1])
 
     for city in cities:
         with open(input_dir / city / "data.lines", 'r') as file:
@@ -144,4 +150,4 @@ def draw_routes(c, input_dir: Path, cities, maxmin):
 
 
 if __name__ == "__main__":
-    draw_poster(Path("."), cities=["ulm"], provider_icon=None)
+    draw_poster(Path("."), cities=["ulm"], provider_icon=None, poster=True)
