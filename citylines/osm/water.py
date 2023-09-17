6a2b66aae3405d0b0200a5f0db22bfd6c9ab5050
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-09-17 08:53:34 +0200

    fix water relation extraction from osm

--- citylines/osm/water.py
+++ citylines/osm/water.py
@@ -1,32 +1,47 @@
+from itertools import chain
+
 import requests
 
 from citylines.gtfs.gtfs import BoundingBox, coord2px
 
 
-def get_osm_water_bodies(bbox: BoundingBox):
+def get_osm_water_bodies(bbox: BoundingBox) -> list[dict]:
     overpass_url = "https://overpass-api.de/api/interpreter"
     query = f"""
     [out:json][bbox:{bbox.bottom},{bbox.left},{bbox.top},{bbox.right}];
     (
       relation["natural"="water"]["water"~"lake|river|pond|reservoir|stream|canal"];
-      way(r:"outer");
+      way(r);
     );
-    out body;
+    out tags body;
     >;
-    out skel qt;
+    out tags skel qt;
     """
     response = requests.get(overpass_url, params={'data': query})
     data = response.json()
-    flat_ways = []
+    relations = []
+    way_dict = {}
     node_dict = {}
+    # first nodes
     for n in data["elements"]:
         if n["type"] == "node":
             px = coord2px(n["lat"], n["lon"], bbox)
             node_dict[n["id"]] = px
-    for way in data["elements"]:
-        if way["type"] == "way":
-            way_nodes = [node_dict[n_id] for n_id in way["nodes"]]
-            flat_ways.append(way_nodes)
+    # then ways
+    for w in data["elements"]:
+        if w["type"] == "way":
+            way_nodes = [node_dict[n_id] for n_id in w["nodes"]]
+            way_dict[w["id"]] = way_nodes
+    # finally reconstruct relations
+    for r in data["elements"]:
+        if r["type"] == "relation":
+            r_ways = []
+            for w in r["members"]:
+                if w["type"] == "way" and w["role"] == "outer":
+                    if w["ref"] in way_dict:
+                        r_ways.append(way_dict[w["ref"]])
+            # flatten
+            r_ways = list(chain.from_iterable(r_ways))
+            relations.append({"name": r["tags"].get("name"), "nodes": r_ways})
 
-    # convert ways to
-    return flat_ways
+    return relations
