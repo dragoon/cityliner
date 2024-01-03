7bc944d5dea2538a57bbfadbfa4113cdbd00fdea
Roman Prokofyev <roman.prokofyev@gmail.com>
2024-01-03 21:14:56 +0100

    extract admin border from osm data via overpass

---
+++ citylines/admin/geocode.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def get_place_relation_id(lat, lon) -> str | None:
+    # Base URL for Nominatim API
+    url = "https://nominatim.openstreetmap.org/reverse"
+
+    # Parameters for the API request
+    params = {
+        "format": "json",
+        "lat": lat,
+        "lon": lon,
+        "zoom": 10,
+        "addressdetails": 1
+    }
+
+    # Sending the GET request
+    response = requests.get(url, params=params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        # Extracting the place name
+        if data['osm_type'] == 'relation':
+            return data['osm_id']
+    else:
+        print("Error:", response.status_code, response.reason)
+    return None
